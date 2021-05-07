2a24b2bbde664e17279ed658b4686c619326eba2
nagendrakodi <nagendrakodi@outlook.com>
2021-05-07 10:00:35 +0530

    Reformated code in PHPStrom with magento-coding-standards, PHPCS, and PHPCB.

--- CartUpdate/Block/Cart/Item/Renderer/UpdateConfigurable.php
+++ CartUpdate/Block/Cart/Item/Renderer/UpdateConfigurable.php
@@ -3,20 +3,20 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Orange\CartUpdate\Block\Cart\Item\Renderer;
 
-use Magento\Catalog\Model\Config\Source\Product\Thumbnail as ThumbnailSource;
-use Magento\Checkout\Block\Cart\Item\Renderer;
-use Magento\Framework\DataObject\IdentityInterface;
-use Magento\Catalog\Pricing\Price\ConfiguredPriceInterface;
-use Magento\Checkout\Block\Cart\Item\Renderer\Actions;
-use Magento\Framework\Pricing\PriceCurrencyInterface;
-use Magento\Framework\View\Element\AbstractBlock;
-use Magento\Framework\View\Element\Message\InterpretationStrategyInterface;
-use Magento\Quote\Model\Quote\Item\AbstractItem;
-use Magento\Framework\App\ObjectManager;
+use Magento\Catalog\Block\Product\ImageBuilder;
+use Magento\Catalog\Helper\Product\Configuration;
 use Magento\Catalog\Model\Product\Configuration\Item\ItemResolverInterface;
+use Magento\Checkout\Model\Session;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Module\Manager;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Url\Helper\Data;
+use Magento\Framework\View\Element\Message\InterpretationStrategyInterface;
+use Magento\Framework\View\Element\Template\Context;
 
 /**
  * Shopping cart item render block for configurable products.
@@ -26,18 +26,18 @@ use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
  */
 class UpdateConfigurable extends \Magento\ConfigurableProduct\Block\Cart\Item\Renderer\Configurable
 {
-    /** @var Configurable **/
+    /** @var Configurable * */
     public $configurable;
 
     /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Catalog\Helper\Product\Configuration $productConfig
-     * @param \Magento\Checkout\Model\Session $checkoutSession
-     * @param \Magento\Catalog\Block\Product\ImageBuilder $imageBuilder
-     * @param \Magento\Framework\Url\Helper\Data $urlHelper
-     * @param \Magento\Framework\Message\ManagerInterface $messageManager
+     * @param Context $context
+     * @param Configuration $productConfig
+     * @param Session $checkoutSession
+     * @param ImageBuilder $imageBuilder
+     * @param Data $urlHelper
+     * @param ManagerInterface $messageManager
      * @param PriceCurrencyInterface $priceCurrency
-     * @param \Magento\Framework\Module\Manager $moduleManager
+     * @param Manager $moduleManager
      * @param InterpretationStrategyInterface $messageInterpretationStrategy
      * @param array $data
      * @param ItemResolverInterface|null $itemResolver
@@ -46,19 +46,20 @@ class UpdateConfigurable extends \Magento\ConfigurableProduct\Block\Cart\Item\Re
      * @codeCoverageIgnore
      */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Catalog\Helper\Product\Configuration $productConfig,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Catalog\Block\Product\ImageBuilder $imageBuilder,
-        \Magento\Framework\Url\Helper\Data $urlHelper,
-        \Magento\Framework\Message\ManagerInterface $messageManager,
+        Context $context,
+        Configuration $productConfig,
+        Session $checkoutSession,
+        ImageBuilder $imageBuilder,
+        Data $urlHelper,
+        ManagerInterface $messageManager,
         PriceCurrencyInterface $priceCurrency,
-        \Magento\Framework\Module\Manager $moduleManager,
+        Manager $moduleManager,
         InterpretationStrategyInterface $messageInterpretationStrategy,
         array $data = [],
         ItemResolverInterface $itemResolver = null,
         Configurable $configurable
-    ) {
+    )
+    {
         parent::__construct(
             $context,
             $productConfig,
@@ -66,7 +67,7 @@ class UpdateConfigurable extends \Magento\ConfigurableProduct\Block\Cart\Item\Re
             $imageBuilder,
             $urlHelper,
             $messageManager,
-            $priceCurrency,           
+            $priceCurrency,
             $moduleManager,
             $messageInterpretationStrategy,
             $data,
--- CartUpdate/Observer/UpdateOptions.php
+++ CartUpdate/Observer/UpdateOptions.php
@@ -2,39 +2,42 @@
 
 namespace Orange\CartUpdate\Observer;
 
-class UpdateOptions implements \Magento\Framework\Event\ObserverInterface
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\DataObject;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class UpdateOptions implements ObserverInterface
 {
     public function __construct(
-        \Magento\Framework\App\RequestInterface $request
+        RequestInterface $request
     )
     {
         $this->_request = $request;
     }
 
-	public function execute(\Magento\Framework\Event\Observer $observer)
-	{
+    public function execute(Observer $observer)
+    {
         $cart = $observer->getCart();
         $data = $observer->getEvent()->getInfo()->toArray();
         $cartData = $this->_request->getPost();
-        foreach ($cartData['cart'] as $itemId => $itemInfo) 
-        {
+        foreach ($cartData['cart'] as $itemId => $itemInfo) {
             $item = $cart->getQuote()->getItemById($itemId);
-            if (!$item) 
+            if (!$item)
                 continue;
 
-            if (!isset($itemInfo['super_attribute']) or empty($itemInfo['super_attribute'])) 
+            if (!isset($itemInfo['super_attribute']) or empty($itemInfo['super_attribute']))
                 continue;
 
-            if ($item->getProduct()->getTypeId() == 'configurable')
-            {
+            if ($item->getProduct()->getTypeId() == 'configurable') {
                 $params = array(
                     'id' => $itemId,
                     'qty' => $itemInfo['qty'],
                     'product' => $item->getProduct()->getId(),
                     'super_attribute' => $itemInfo['super_attribute']
                 );
-                $cart->updateItem($itemId, new \Magento\Framework\DataObject($params));
+                $cart->updateItem($itemId, new DataObject($params));
             }
-        } 
-    }       
+        }
+    }
 }
